8442a301c1e559d8db775dc9f90bed08dccc4b10
Matthias Gumpinger <79538390+matthiasgumpinger@users.noreply.github.com>
2022-01-30 18:33:55 +0100

    Neues Package erstellt

---
+++ .idea/net/htlgkr/berghammert/Figure.java
@@ -0,0 +1,28 @@
+package net.htlgkr.berghammert;
+
+import java.util.LinkedList;
+
+public class Figure {
+    int x;
+    int y;
+    boolean isWhite;
+    LinkedList<Figure> f;
+    public Figure(int x, int y, boolean isWhite, LinkedList<Figure> f) {
+        this.x = x;
+        this.y = y;
+        this.isWhite = isWhite;
+        this.f = f;
+        f.add(this);
+    }
+    public void move(int x, int y) {
+        for(Figure f: f) {
+            if(f.x == x && f.y == y) {
+                f.beat();
+            }
+        }
+    }
+    public void beat() {
+        f.remove(this);
+    }
+
+}
