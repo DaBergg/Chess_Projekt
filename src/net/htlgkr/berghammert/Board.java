3715b3ac61b99eda79591b5c894e722c0b59ca05
DaBergg <BerghammerT190006@sus.htl-grieskirchen.at>
2022-02-03 22:00:47 +0100

    Update Board.java

--- src/net/htlgkr/berghammert/Board.java
+++ src/net/htlgkr/berghammert/Board.java
@@ -3,206 +3,340 @@ package net.htlgkr.berghammert;
 import java.awt.*;
 import java.awt.event.*;
 import java.util.*;
+import java.util.concurrent.CopyOnWriteArrayList;
 import javax.swing.*;
 
-public class Board extends JFrame implements MouseListener, MouseMotionListener {
-    JLayeredPane layeredPane;
-    JPanel chessBoard;
-    JLabel chessPiece;
-    int xAdjustment;
-    int yAdjustment;
+public class Board
+{
+    public static final char rowMax='8', colMax='h', rowMin='1', colMin='a';
+    public static final String White="White", Black = "Black";
+    private Movement movement;
+    private Cell cells[][];
+    private String currentPlayerColour;
+    private boolean selected;
+    private King whiteKing, blackKing;
+    private Cell selectedCell;
+    private CopyOnWriteArrayList<Piece> whitePieces, blackPieces,
+            killedPieces;
 
-    public Board(){
-        Dimension boardSize = new Dimension(600, 600);
+    private void emptyBoard()
+    {
+        currentPlayerColour = Board.White;
+        selected = false;
+        selectedCell = null;
+        whitePieces = new CopyOnWriteArrayList<>();
+        blackPieces = new CopyOnWriteArrayList<>();
+        killedPieces = new CopyOnWriteArrayList<>();
+        try
+        {
+            cells = new Cell[8][8];
+            for(int i=0; i<8; i++)
+            {	for(int j=0; j<8; j++)
+            {	cells[i][j]= new Cell((char)(rowMin+i), (char)(colMin+j));
+            }
+            }
 
-// Use a Layered Pane for this this application
-        layeredPane = new JLayeredPane();
-        getContentPane().add(layeredPane);
-        layeredPane.setPreferredSize(boardSize);
-        layeredPane.addMouseListener(this);
-        layeredPane.addMouseMotionListener(this);
+            movement = new Movement(this);
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
 
-//Add a chess board to the Layered Pane
 
-        chessBoard = new JPanel();
-        layeredPane.add(chessBoard, JLayeredPane.DEFAULT_LAYER);
-        chessBoard.setLayout( new GridLayout(8, 8) );
-        chessBoard.setPreferredSize( boardSize );
-        chessBoard.setBounds(0, 0, boardSize.width, boardSize.height);
+    public Board()
+    {
+        this.emptyBoard();
+    }
 
-        for (int i = 0; i < 64; i++) {
-            JPanel square = new JPanel( new BorderLayout() );
-            chessBoard.add( square );
 
-            int row = (i / 8) % 2;
-            if (row == 0)
-                square.setBackground( i % 2 == 0 ? Color.lightGray : Color.white );
-            else
-                square.setBackground( i % 2 == 0 ? Color.white : Color.lightGray );
+    public Board(Boolean x)
+    {
+        this.emptyBoard();
+        for(int j=0; j<8; j++)
+        {
+            try
+            {
+                this.construct(new Pawn(White, cells[1][j]), cells[1][j]);
+                //the second nearest row to white is filled with white pawns
+                this.construct(new Pawn(Black, cells[6][j]), cells[6][j]);
+                //the second nearest row to black is filled with black pawns.
+            }
+            catch(Exception e)
+            {
+                System.out.println("Something went wrong while constructing pawns.");
+            }
         }
 
-//Add a few pieces to the board
-
-        JLabel piece = new JLabel( new ImageIcon("C:\\Users\\Thomas Berghammer\\Documents\\GitHub\\Chess_Projekt\\src\\Figuren\\brook.png") );
-        JPanel panel = (JPanel)chessBoard.getComponent(0);
-        panel.add(piece);
-        piece = new JLabel(new ImageIcon("C:\\Users\\Thomas Berghammer\\Documents\\GitHub\\Chess_Projekt\\src\\Figuren\\brook.png"));
-        panel = (JPanel)chessBoard.getComponent(7);
-        panel.add(piece);
-        piece = new JLabel(new ImageIcon("C:\\Users\\Thomas Berghammer\\Documents\\GitHub\\Chess_Projekt\\src\\Figuren\\bknight.png"));
-        panel = (JPanel)chessBoard.getComponent(1);
-        panel.add(piece);
-        piece = new JLabel(new ImageIcon("C:\\Users\\Thomas Berghammer\\Documents\\GitHub\\Chess_Projekt\\src\\Figuren\\bknight.png"));
-        panel = (JPanel)chessBoard.getComponent(6);
-        panel.add(piece);
-        piece = new JLabel(new ImageIcon("C:\\Users\\Thomas Berghammer\\Documents\\GitHub\\Chess_Projekt\\src\\Figuren\\bbishop.png"));
-        panel = (JPanel)chessBoard.getComponent(2);
-        panel.add(piece);
-        piece = new JLabel(new ImageIcon("C:\\Users\\Thomas Berghammer\\Documents\\GitHub\\Chess_Projekt\\src\\Figuren\\bbishop.png"));
-        panel = (JPanel)chessBoard.getComponent(5);
-        panel.add(piece);
-        piece = new JLabel(new ImageIcon("C:\\Users\\Thomas Berghammer\\Documents\\GitHub\\Chess_Projekt\\src\\Figuren\\bqueen.png"));
-        panel = (JPanel)chessBoard.getComponent(3);
-        panel.add(piece);
-        piece = new JLabel(new ImageIcon("C:\\Users\\Thomas Berghammer\\Documents\\GitHub\\Chess_Projekt\\src\\Figuren\\bking.png"));
-        panel = (JPanel)chessBoard.getComponent(4);
-        panel.add(piece);
-        piece = new JLabel(new ImageIcon("C:\\Users\\Thomas Berghammer\\Documents\\GitHub\\Chess_Projekt\\src\\Figuren\\bpawn.png"));
-        panel = (JPanel)chessBoard.getComponent(8);
-        panel.add(piece);
-        piece = new JLabel(new ImageIcon("C:\\Users\\Thomas Berghammer\\Documents\\GitHub\\Chess_Projekt\\src\\Figuren\\bpawn.png"));
-        panel = (JPanel)chessBoard.getComponent(9);
-        panel.add(piece);
-        piece = new JLabel(new ImageIcon("C:\\Users\\Thomas Berghammer\\Documents\\GitHub\\Chess_Projekt\\src\\Figuren\\bpawn.png"));
-        panel = (JPanel)chessBoard.getComponent(10);
-        panel.add(piece);
-        piece = new JLabel(new ImageIcon("C:\\Users\\Thomas Berghammer\\Documents\\GitHub\\Chess_Projekt\\src\\Figuren\\bpawn.png"));
-        panel = (JPanel)chessBoard.getComponent(11);
-        panel.add(piece);
-        piece = new JLabel(new ImageIcon("C:\\Users\\Thomas Berghammer\\Documents\\GitHub\\Chess_Projekt\\src\\Figuren\\bpawn.png"));
-        panel = (JPanel)chessBoard.getComponent(12);
-        panel.add(piece);
-        piece = new JLabel(new ImageIcon("C:\\Users\\Thomas Berghammer\\Documents\\GitHub\\Chess_Projekt\\src\\Figuren\\bpawn.png"));
-        panel = (JPanel)chessBoard.getComponent(13);
-        panel.add(piece);
-        piece = new JLabel(new ImageIcon("C:\\Users\\Thomas Berghammer\\Documents\\GitHub\\Chess_Projekt\\src\\Figuren\\bpawn.png"));
-        panel = (JPanel)chessBoard.getComponent(14);
-        panel.add(piece);
-        piece = new JLabel(new ImageIcon("C:\\Users\\Thomas Berghammer\\Documents\\GitHub\\Chess_Projekt\\src\\Figuren\\bpawn.png"));
-        panel = (JPanel)chessBoard.getComponent(15);
-        panel.add(piece);
-        piece = new JLabel(new ImageIcon("C:\\Users\\Thomas Berghammer\\Documents\\GitHub\\Chess_Projekt\\src\\Figuren\\wrook.png"));
-        panel = (JPanel)chessBoard.getComponent(63);
-        panel.add(piece);
-        piece = new JLabel(new ImageIcon("C:\\Users\\Thomas Berghammer\\Documents\\GitHub\\Chess_Projekt\\src\\Figuren\\wrook.png"));
-        panel = (JPanel)chessBoard.getComponent(56);
-        panel.add(piece);
-        piece = new JLabel(new ImageIcon("C:\\Users\\Thomas Berghammer\\Documents\\GitHub\\Chess_Projekt\\src\\Figuren\\wknight.png"));
-        panel = (JPanel)chessBoard.getComponent(57);
-        panel.add(piece);
-        piece = new JLabel(new ImageIcon("C:\\Users\\Thomas Berghammer\\Documents\\GitHub\\Chess_Projekt\\src\\Figuren\\wknight.png"));
-        panel = (JPanel)chessBoard.getComponent(62);
-        panel.add(piece);
-        piece = new JLabel(new ImageIcon("C:\\Users\\Thomas Berghammer\\Documents\\GitHub\\Chess_Projekt\\src\\Figuren\\wbishop.png"));
-        panel = (JPanel)chessBoard.getComponent(58);
-        panel.add(piece);
-        piece = new JLabel(new ImageIcon("C:\\Users\\Thomas Berghammer\\Documents\\GitHub\\Chess_Projekt\\src\\Figuren\\wbishop.png"));
-        panel = (JPanel)chessBoard.getComponent(61);
-        panel.add(piece);
-        piece = new JLabel(new ImageIcon("C:\\Users\\Thomas Berghammer\\Documents\\GitHub\\Chess_Projekt\\src\\Figuren\\wqueen.png"));
-        panel = (JPanel)chessBoard.getComponent(60);
-        panel.add(piece);
-        piece = new JLabel(new ImageIcon("C:\\Users\\Thomas Berghammer\\Documents\\GitHub\\Chess_Projekt\\src\\Figuren\\wking.png"));
-        panel = (JPanel)chessBoard.getComponent(59);
-        panel.add(piece);
-        piece = new JLabel(new ImageIcon("C:\\Users\\Thomas Berghammer\\Documents\\GitHub\\Chess_Projekt\\src\\Figuren\\wpawn.png"));
-        panel = (JPanel)chessBoard.getComponent(55);
-        panel.add(piece);
-        piece = new JLabel(new ImageIcon("C:\\Users\\Thomas Berghammer\\Documents\\GitHub\\Chess_Projekt\\src\\Figuren\\wpawn.png"));
-        panel = (JPanel)chessBoard.getComponent(54);
-        panel.add(piece);
-        piece = new JLabel(new ImageIcon("C:\\Users\\Thomas Berghammer\\Documents\\GitHub\\Chess_Projekt\\src\\Figuren\\wpawn.png"));
-        panel = (JPanel)chessBoard.getComponent(53);
-        panel.add(piece);
-        piece = new JLabel(new ImageIcon("C:\\Users\\Thomas Berghammer\\Documents\\GitHub\\Chess_Projekt\\src\\Figuren\\wpawn.png"));
-        panel = (JPanel)chessBoard.getComponent(52);
-        panel.add(piece);
-        piece = new JLabel(new ImageIcon("C:\\Users\\Thomas Berghammer\\Documents\\GitHub\\Chess_Projekt\\src\\Figuren\\wpawn.png"));
-        panel = (JPanel)chessBoard.getComponent(51);
-        panel.add(piece);
-        piece = new JLabel(new ImageIcon("C:\\Users\\Thomas Berghammer\\Documents\\GitHub\\Chess_Projekt\\src\\Figuren\\wpawn.png"));
-        panel = (JPanel)chessBoard.getComponent(50);
-        panel.add(piece);
-        piece = new JLabel(new ImageIcon("C:\\Users\\Thomas Berghammer\\Documents\\GitHub\\Chess_Projekt\\src\\Figuren\\wpawn.png"));
-        panel = (JPanel)chessBoard.getComponent(49);
-        panel.add(piece);
-        piece = new JLabel(new ImageIcon("C:\\Users\\Thomas Berghammer\\Documents\\GitHub\\Chess_Projekt\\src\\Figuren\\wpawn.png"));
-        panel = (JPanel)chessBoard.getComponent(48);
-        panel.add(piece);
-    }
-
-    public void mousePressed(MouseEvent e){
-        chessPiece = null;
-        Component c = chessBoard.findComponentAt(e.getX(), e.getY());
-
-        if (c instanceof JPanel)
-            return;
-
-        Point parentLocation = c.getParent().getLocation();
-        xAdjustment = parentLocation.x - e.getX();
-        yAdjustment = parentLocation.y - e.getY();
-        chessPiece = (JLabel)c;
-        chessPiece.setLocation(e.getX() + xAdjustment, e.getY() + yAdjustment);
-        chessPiece.setSize(chessPiece.getWidth(), chessPiece.getHeight());
-        layeredPane.add(chessPiece, JLayeredPane.DRAG_LAYER);
-    }
-
-//Move the chess piece around
-
-    public void mouseDragged(MouseEvent me) {
-        if (chessPiece == null) return;
-        chessPiece.setLocation(me.getX() + xAdjustment, me.getY() + yAdjustment);
-    }
-
-//Drop the chess piece back onto the chess board
-
-    public void mouseReleased(MouseEvent e) {
-        if(chessPiece == null) return;
-
-        chessPiece.setVisible(false);
-        Component c = chessBoard.findComponentAt(e.getX(), e.getY());
-
-        if (c instanceof JLabel){
-            Container parent = c.getParent();
-            parent.remove(0);
-            parent.add( chessPiece );
+        try
+        {
+            //giving 2 rooks to both the player on respective corners.
+            this.construct(new Rook(White), cells[0][0]);
+            this.construct(new Rook(White), cells[0][7]);
+            this.construct(new Rook(Black), cells[7][0]);
+            this.construct(new Rook(Black), cells[7][7]);
         }
-        else {
-            Container parent = (Container)c;
-            parent.add( chessPiece );
+        catch(Exception e)
+        {
+            System.out.println("Something went wrong while constructing rooks.");
         }
 
-        chessPiece.setVisible(true);
+        try
+        {
+            //giving 2 knights to both players on cells just beside the rooks.
+            this.construct(new Knight(White), cells[0][1]);
+            this.construct(new Knight(White), cells[0][6]);
+            this.construct(new Knight(Black), cells[7][1]);
+            this.construct(new Knight(Black), cells[7][6]);
+        }
+        catch(Exception e)
+        {
+            System.out.println("Something went wrong while constructing knights.");
+        }
+
+        try
+        {	//giving 2 bishops to both players on cells just beside the knights.
+            this.construct(new Bishop(White), cells[0][2]);
+            this.construct(new Bishop(White), cells[0][5]);
+            this.construct(new Bishop(Black), cells[7][2]);
+            this.construct(new Bishop(Black), cells[7][5]);
+        }
+        catch(Exception e)
+        {
+            System.out.println("Something went wrong while constructing bishops.");
+        }
+
+        try
+        {
+            //giving a queen to both players on cells just beside a bishop.
+            this.construct(new Queen(White), cells[0][3]);
+            this.construct(new Queen(Black), cells[7][3]);
+        }
+        catch(Exception e)
+        {
+            System.out.println("Something went wrong while constructing queens.");
+        }
+
+        try
+        {
+            //giving a king to both players on cells just beside the queen.
+            whiteKing = new King(White);
+            blackKing = new King(Black);
+            this.construct(whiteKing, cells[0][4]);
+            this.construct(blackKing, cells[7][4]);
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+
     }
 
-    public void mouseClicked(MouseEvent e) {
-
-    }
-    public void mouseMoved(MouseEvent e) {
-    }
-    public void mouseEntered(MouseEvent e){
-
-    }
-    public void mouseExited(MouseEvent e) {
-
+    public String getCurrentTurn()
+    {
+        return this.currentPlayerColour;
     }
 
-    public static void main(String[] args) {
-        JFrame frame = new Board();
-        frame.setDefaultCloseOperation(DISPOSE_ON_CLOSE );
-        frame.pack();
-        frame.setResizable(true);
-        frame.setLocationRelativeTo( null );
-        frame.setVisible(true);
+    public void flipTurn()
+    {
+        this.currentPlayerColour = opposite(currentPlayerColour);
     }
+
+    public void construct(Piece piece, Cell cell)
+    {
+
+        if(piece.colour.equals(White))
+            whitePieces.add(piece);
+        else
+            blackPieces.add(piece);
+        movement.construct(piece, cell);
+    }
+
+    /**
+     * Returns the reference to the cell at (row, col) in the board.
+     * Returns null if the row or column are not valid (out of bounds).
+     * */
+    public Cell getCellAt(char row, char col)
+    {
+        if(row>=rowMin && col>=colMin && row<=rowMax && col<=colMax)
+            return cells[row-rowMin][col-colMin];
+        else
+            return null;
+    }
+
+    public Cell getCellAt(int row, int col)
+    {
+        return this.getCellAt((char)row, (char)col);
+    }
+
+    public CopyOnWriteArrayList<Piece> getPieces(String colour)
+    {
+        if(colour.equals(White))
+            return whitePieces;
+        else
+            return blackPieces;
+    }
+
+    public static String opposite(String colour)
+    {
+        if(colour.equals(White))
+            return Black;
+        else
+            return White;
+    }
+
+    public void kill(Piece piece)
+    {
+        if(piece!= null)
+        {
+            killedPieces.add(piece);
+        }
+    }
+
+    public boolean isKilled(Piece piece)
+    {
+        //System.out.println(piece + " isKilled");
+        if(piece == null)
+            return true;
+        return killedPieces.contains(piece);
+    }
+
+    public void reincarnate(Piece piece)
+    {
+        if(piece!= null)
+        {
+            killedPieces.remove(piece);
+            //if(piece.getColour() == White)
+            //	whitePieces.add(piece);
+            //else
+            //	blackPieces.add(piece);
+        }
+    }
+
+
+
+    public boolean clicked(int row, int col)
+    {
+        return this.clicked((char)row, (char)col);
+    }
+
+    public boolean clicked(char row, char col)
+    {
+        Cell thisCell = this.getCellAt(row, col);
+        if(thisCell == null)
+        {
+            System.out.println("Invalid cell in clicked() of Board");
+            return false;
+        }
+
+        //Piece piece = thisCell.getPiece();
+
+        if(!selected)
+        {
+            //if player clicks on his own piece, mark it as selected.
+            if(movement.canSelect(thisCell, currentPlayerColour))
+            {
+                selectedCell = thisCell;
+                selected = true;
+                thisCell.select(true);
+
+                //pinning: a piece can't move if it makes
+                //it's own king vulnerable in the immediate next move.
+
+                ArrayList<Cell> allMoves = movement.getAllMoves(thisCell);
+                for(Cell c: allMoves)
+                {
+                    c.setNextMove(true);
+                }
+            }
+            //If he clicks on an opponent piece, don't do anything.
+            return false;
+        }
+        else
+        {
+            selected = false;
+            selectedCell.select(false);
+            if(selectedCell != null)
+            {
+                ArrayList<Cell> allMoves = movement.getAllMoves(selectedCell);
+                for(Cell dest: allMoves)
+                {
+                    dest.setNextMove(false);
+                }
+
+                boolean move = false;
+                String moveString = null;
+                if(moveString == null)
+                {
+                    Move temp = movement.moveTo(selectedCell, thisCell);
+                    if(temp != null)
+                        moveString = temp.toString();
+
+                }
+                if(moveString != null)//the player has made a move.
+                {
+                    move = true;
+                    this.flipTurn();
+                    System.out.println(moveString);
+                }
+                return move;
+            }
+            selectedCell = null;
+            return false;
+        }
+    }
+
+    public King getKing(String colour)
+    {
+        if( colour.equals(White) )
+            return whiteKing;
+        else
+            return blackKing;
+    }
+
+    public void promotePawn(Pawn pawn, Queen queen, Cell queenCell)
+    {
+        kill(pawn);
+        try
+        {
+            this.construct(queen, queenCell);
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+            System.out.println("Something went wrong in promotePawn() of Board");
+        }
+    }
+
+    public boolean isCheckMate(String playerColour)
+    {
+        return movement.isCheckMate(playerColour);
+    }
+
+    public boolean isUnderCheck(String playerColour)
+    {
+        return movement.isUnderCheck(playerColour);
+    }
+
+    public String getPieceType(Cell thisCell)
+    {
+        return movement.getPieceType(thisCell);
+    }
+
+    public Class<? extends Piece> getPieceClass(Cell thisCell)
+    {
+        return movement.getPieceClass(thisCell);
+    }
+	/*
+	public ArrayList<Cell> getAllMoves(Piece piece)
+	{
+		return movement.getAllMoves(piece);
+	}*/
+
+    public Movement getMovement()
+    {
+        return movement;
+    }
+
 }
