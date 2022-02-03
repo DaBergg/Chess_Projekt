bd038f650c299b827a8771023029c916f18844d4
DaBergg <BerghammerT190006@sus.htl-grieskirchen.at>
2022-02-03 21:49:16 +0100

    Create Cell.java

---
+++ src/net/htlgkr/berghammert/Cell.java
@@ -0,0 +1,60 @@
+package net.htlgkr.berghammert;
+
+public class Cell {
+    public final char row;
+    public final char col;
+    private boolean selected;
+    private boolean nextMove;
+
+
+    public Cell(char r, char c) throws Exception
+    {
+        row = r;
+        col = c;
+
+        selected = false;
+        nextMove = false;
+    }
+
+
+    public void select(boolean x)
+    {
+        this.selected = x;
+    }
+
+
+    public void setNextMove(boolean x)
+    {
+        nextMove = x;
+    }
+
+
+    public boolean isSelected()
+    {
+        return selected;
+    }
+
+
+    public boolean isNextMove()
+    {
+        return nextMove;
+    }
+
+
+    @Override
+    public String toString()
+    {
+        return this.col+""+this.row;
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if(!(obj instanceof Cell))
+            return false;
+        Cell other = (Cell)obj;
+        if(other.row == this.row && other.col == this.col)
+            return true;
+        return false;
+    }
+}
