5d5ebcc57a2a0759923e81bf4a76208c2ca2d4ea
DaBergg <BerghammerT190006@sus.htl-grieskirchen.at>
2022-02-05 18:20:40 +0100

    Create Queen.java

---
+++ src/net/htlgkr/berghammert/Queen.java
@@ -0,0 +1,16 @@
+package net.htlgkr.berghammert;
+
+public class Queen extends Figure
+{
+    public Queen(String col) throws Exception
+    {	super(col);
+    }
+
+    @Override
+    public String toString()
+    {
+        return colour.charAt(0)+"Q";
+    }
+
+}
+
